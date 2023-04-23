665a6b108e617e176aa7eb415af694a5ed4bfac6
Ikyrillos <kyrillosmaherfekri@gmail.com>
2023-04-23 17:06:00 +0200

    dispose: controllers of login auth

--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -16,6 +16,13 @@ class _LoginViewState extends State<LoginView> {
   final appBar = UiConstants.appBar();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
